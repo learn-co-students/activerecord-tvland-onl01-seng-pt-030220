b36b58f4086ff9200009e993dcfe3f398013bebe
AHMED AFANDY <eng.computer1@yahoo.com>
2020-05-10 00:13:37 +0000

    Done.

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,13 @@
 class Character < ActiveRecord::Base
-  
+  belongs_to :actor
+  belongs_to :show
+
+  def say_that_thing_you_say
+    "#{self.name} always says: #{self.catchphrase}"
+  end
+
+  def build_show(hash_attr)
+      self.show = Show.create(atthash_attrr)
+  end
+
 end

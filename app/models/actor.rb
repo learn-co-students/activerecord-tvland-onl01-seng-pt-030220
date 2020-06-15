f43b4b6d76393bee883bd4ad763c9ca3ec4b35f4
VQuine <01010110.v@gmail.com>
2020-06-15 19:01:31 -0400

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -3,7 +3,14 @@ class Actor < ActiveRecord::Base
   has_many :shows, through: :characters
 
   def full_name
-    "#{self.first_name} #{self.last_name}"
+    # "#{self.first_name} #{self.last_name}" STRING INTERPOLATIONS ARE BAD..mostly..ithink. I don't know don't ask me
+    self.first_name + " " + self.last_name
+  end
+
+  def list_roles
+    self.characters.map do |character|
+      character.name + " - " + character.show.name
+    end
   end
 
 end 
